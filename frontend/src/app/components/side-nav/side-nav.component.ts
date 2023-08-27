ab6fc291c8c1538df0e05aeb16fa94c78f14777e
ahnsunnyyoung <ahnsunnyyoung@gmail.com>
2023-08-27 18:13:15 +0100

    modulized side-nav with Service, Oninit

--- frontend/src/app/components/side-nav/side-nav.component.ts
+++ frontend/src/app/components/side-nav/side-nav.component.ts
@@ -1,11 +1,103 @@
-import { Component, Input } from '@angular/core';
+import { Component, Injectable, Input, OnInit } from '@angular/core';
+
+
+@Injectable()
+class SideNavService {
+
+  public getSideNavData(userType:String): Array<NavigationData>{
+    switch(userType){
+      case UserType.Advertiser: {
+        return [
+          {
+            "routerLink":"/advertisers/dashboard",
+            "title":"Dashboard",
+          },
+          {
+            "routerLink":"/advertisers/manage",
+            "title":"Manage ADs",
+          },
+          {
+            "routerLink":"/advertisers/support",
+            "title":"Support",
+          },
+          {
+            "routerLink":"/advertisers/logout",
+            "title":"Logout",
+          },
+        ]
+      }
+      case UserType.Driver: {
+        return [
+          {
+            "routerLink":"/drivers/dashboard",
+            "title":"Dashboard",
+          },
+          {
+            "routerLink":"/drivers/manage",
+            "title":"Manage Taxis",
+          },
+          {
+            "routerLink":"/drivers/support",
+            "title":"Support",
+          },
+          {
+            "routerLink":"/drivers/logout",
+            "title":"Logout",
+          },
+        ]
+      }
+      default: {
+        return [
+          {
+            "routerLink":"/drivers/dashboard",
+            "title":"Dashboard",
+          },
+          {
+            "routerLink":"/drivers/manage",
+            "title":"Manage Taxis",
+          },
+          {
+            "routerLink":"/drivers/support",
+            "title":"Support",
+          },
+          {
+            "routerLink":"/drivers/logout",
+            "title":"Logout",
+          },
+        ]
+      }
+    }
+  }
+}
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
+  providers: [SideNavService]
 })
-export class SideNavComponent {
+export class SideNavComponent implements OnInit{
   @Input() userType!: string;
+  public navData: Array<NavigationData> | undefined;
 
+
+  constructor(private sideNavService: SideNavService) {
+  }
+
+  ngOnInit(): void {
+    this.navData = this.sideNavService.getSideNavData(this.userType);
+  }
+  
+
+}
+
+interface NavigationData {
+  routerLink: string;
+  title: string;
+}
+
+
+enum UserType {
+  Advertiser = "advertiser",
+  Driver = "driver",
 }
