942964d052938af2b65256cb7bc2822f267eff36
ahnsunnyyoung <ahnsunnyyoung@gmail.com>
2023-08-27 16:36:18 +0100

    add user model, repo, service

---
+++ backend/src/main/java/com/locatads/backend/repo/UserRepo.java
@@ -0,0 +1,13 @@
+package com.locatads.backend.repo;
+
+import com.locatads.backend.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    void deleteUserById(Long id);
+
+    Optional<User> findUserById(Long id);
+}
